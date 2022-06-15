85722773f2cba1bff3552457e4633f697e558275
kriti <kriti@LAPTOP-40HQLM92>
2022-06-15 00:58:10 -0400

    asfds

--- movie-theater-src/src/main/java/com/jpmc/theater/LocalDateProvider.java
+++ movie-theater-src/src/main/java/com/jpmc/theater/LocalDateProvider.java
@@ -2,9 +2,12 @@ package com.jpmc.theater;
 
 import java.time.LocalDate;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class LocalDateProvider {
     private static LocalDateProvider instance = null;
-
+    private static Logger logger = LogManager.getLogger(LocalDateProvider.class);
     /**
      * @return make sure to return singleton instance
      */
